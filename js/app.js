3c8ce4f6058eb38a3d5825464cb1ffa920d9e95c
Savinu2005 <palansooriyasavinu@gmail.com>
2026-07-19 21:27:32 +0530

    Comment out switch case logic for day in app.js

--- js/app.js
+++ js/app.js
@@ -65,18 +65,25 @@
 
 //-----------------------------------------
 
-let day = 2;
+// let day = 2;
 
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    default:
-        console.log("Invalid day");
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+//-----------------------------------------
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration: " + i);
 }
