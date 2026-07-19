5f8545d2a1f29187b860488f1f61fcb7219fc0db
Savinu2005 <palansooriyasavinu@gmail.com>
2026-07-19 21:11:50 +0530

    Comment out initial console logs and reorganize variable declarations in app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
- console.log("Hello World");
+//  console.log("Hello World");
 
-let name = "John Doe";
+// let name = "John Doe";
 
-let age = 30;
+// let age = 30;
 
-console.log(name);
-console.log(age);
+// console.log(typeof name);
+// console.log(age);
 
-alert("Welcome to my website!");
+// alert("Welcome to my website!");
+
+//----------------------------------------
+
+// let name = "John Doe";
+// let age = 30;
+// let isStudent = true;
+// let customer = [];
+// let customer = {};
+// let salary = 4000.90;
+
+// console.log(typeof name);
+
+/==========================================
+
